9e6e4e46cdfb0cb03557208b40b2c376c7edcdd9
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-10-18 03:00:50 -0700

    completing api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -53,4 +53,49 @@ module.exports = function(app) {
       res.json(data);
     });
   });
+
+  app.put("/delete-from-saved/:articleId", function(req, res) {
+    db.Article.findByIdAndUpdate(req.params.articleId, {
+      $set: { saved: false }
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  app.post("/create-note/:articleId", function(req, res) {
+    console.log(req.body);
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        console.log(dbNote._id);
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.articleId },
+          { $push: { note: dbNote._id } },
+          { new: true }
+        );
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get("/show-article-notes/:articleId", function(req, res) {
+    db.Article.findById(req.params.articleId)
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.delete("/delete-note/:noteId", function(req, res) {
+    db.Note.findByIdAndRemove(req.params.noteId, (err, note) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send();
+    });
+  });
 };
