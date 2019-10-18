4943a4033d695fa604a01e19483308c26d37b260
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-10-18 03:14:25 -0700

    deleting routes folder trying to use mvc and handlebars

--- server.js
+++ server.js
@@ -1,30 +1,63 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const request = require("request");
-const cheerio = require("cheerio");
-const bodyParser = require("body-parser");
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
-var PORT = process.env.PORT || 3030;
+// Initialize Express
+var app = express();
 
-const app = express();
+// Use body parser with our app
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
-app.use(express.static("public"));
+// Make public a static dir
+app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ type: "application/*+json" }));
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+// Database configuration with mongoose
+var databaseUri = "mongodb://localhost/mongoosearticles";
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/LandScraper";
+var db = mongoose.connection;
 
-// mongoose.connect(MONGODB_URI);
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+db.once("open", function() {
+  console.log("Mongoose connection sucessful.");
+});
 
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT);
+//set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var router = express.Router();
+
+// Require routes file pass router object
+require("./config/routes")(router);
+
+// Have every request go through router middlewar
+app.use(router);
+
+//set port
+var port = process.env.PORT || 8080;
+
+//setup listener
+app.listen(port, function() {
+  console.log("app running on port " + port);
 });
