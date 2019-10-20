5758f9213f929c2ca204a97d27e148861f913241
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-10-20 03:48:53 -0700

    fix conection via mongo

--- models/Article.js
+++ models/Article.js
@@ -1,21 +1,35 @@
+// Require mongoose
 var mongoose = require("mongoose");
-
+var Note = require("./Note");
+// Create Schema class
 var Schema = mongoose.Schema;
+
+// Create article schema
 var ArticleSchema = new Schema({
   title: {
     type: String,
     required: true
   },
+  summary: {
+    type: String,
+//    required: true
+  },
   link: {
     type: String,
     required: true
   },
-  comment: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Comment"
-    }
-  ]
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  notes: [{
+     type: Schema.Types.ObjectId,
+     ref: "Note"
+  }]
 });
+
+// Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
 module.exports = Article;
